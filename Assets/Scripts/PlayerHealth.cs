87b0a5200b75f5147e4b6378c9c6e948eeaed063
Antoine Bauchot <bchtantoine@gmail.com>
2025-04-06 10:34:26 +0200

    Health UI element

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
-    public readonly static int MaxHealth = 10;
+    public readonly static int MaxHealth = 5;
     public int currentHealth = MaxHealth;
     public float invincibilityDuration = 1f;
     public float knockbackForce = 10f;
@@ -11,12 +13,19 @@ public class PlayerHealth : MonoBehaviour
     private bool _isInvincible;
     private Rigidbody2D _rigidbody2D;
     private SpriteRenderer _spriteRenderer;
+    
+    [Header("Health UI")]
+    [SerializeField] private List<Image> healthSegments;
+    [SerializeField] private Sprite filledSegment;
+    [SerializeField] private Sprite emptySegment;
+
 
     private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
         currentHealth = MaxHealth;
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateHealthUI();
     }
     
     public void TakeDamage(int damage, Vector2 sourcePosition)
@@ -31,6 +40,7 @@ public class PlayerHealth : MonoBehaviour
             return;
         }
 
+        UpdateHealthUI();
         StartCoroutine(Invincibility());
         ApplyKnockback(sourcePosition);
     }
@@ -38,7 +48,7 @@ public class PlayerHealth : MonoBehaviour
     private void ApplyKnockback(Vector2 sourcePosition)
     {
         Vector2 knockDirection = ((Vector2)transform.position - sourcePosition).normalized;
-        _rigidbody2D.linearVelocity = knockDirection * knockbackForce;
+        _rigidbody2D.AddForce(knockDirection * knockbackForce, ForceMode2D.Impulse);
         Debug.Log("Knockback applied: " + knockDirection);
     }
 
@@ -56,6 +66,14 @@ public class PlayerHealth : MonoBehaviour
 
         _spriteRenderer.enabled = true;
         _isInvincible = false;
+    }
+    
+    private void UpdateHealthUI()
+    {
+        for (int i = 0; i < healthSegments.Count; i++)
+        {
+            healthSegments[i].sprite = i < currentHealth ? filledSegment : emptySegment;
+        }
     }
     
     private static void Die()
